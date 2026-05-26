93df5d9de18e9d5383745bfab337ace6b639599d
Sid294 <siddharth.lakshminarayanan@gmail.com>
2026-05-26 14:13:32 -0500

    modified gemini api key

--- backend/AI/gemini.py
+++ backend/AI/gemini.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from google import genai
 
 
-ROOT_ENV = Path(__file__).resolve().parents[1] / ".env"
+ROOT_ENV = Path(__file__).resolve().parents[2] / ".env"
 load_dotenv(ROOT_ENV)
 
 
@@ -15,16 +15,33 @@ def main():
         raise RuntimeError("GEMINI_API_KEY is not set. Add it to the repository .env file.")
 
     client = genai.Client(api_key=api_key)
-    prompt = input("Enter your prompt: ")
-    response = client.models.generate_content(
+    chat = client.chats.create(
         model="gemini-3.1-flash-lite",
         config={
-            "system_instruction": "You are a helpful assistant that provides concise and accurate information about cybersecurity. You are are a friendly mentor who is always eager to share knowledge and help others understand complex cybersecurity concepts in a simple way. You can provide explanations, tips, and best practices related to cybersecurity topics.",
-        },
-        contents=prompt,
+            "system_instruction": "You are a helpful assistant that provides concise and accurate information about cybersecurity. You are a friendly mentor who is always eager to share knowledge and help others understand complex cybersecurity concepts in a simple way. You can provide explanations, tips, and best practices related to cybersecurity topics.",
+        }
     )
+    while True:
+        try:
+            prompt = input("Enter your prompt: ")
+            # stop on empty input
+            decide = client.models.generate_content(
+                model="gemini-3.1-flash-lite",
+                config={
+                    "system_instruction": "Provide only a yes or no answer."
+                },
+                contents=prompt + "do you want to continue the conversation?"
+            )
+            decide = decide.text.strip().upper()
+            if decide == "YES" or decide == "NO":
+                break
+            result = chat.send_message(prompt)
+            print(result.text)
+        except:
+            print("You ran out of API credits, please upgrade your plan to continue or wait until the quota resets.")
+            break
 
-    print(response.text)
+
 
 
 if __name__ == "__main__":
